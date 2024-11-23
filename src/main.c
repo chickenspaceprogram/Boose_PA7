76f5407507a050298d8736167773531565ab7828
Athena Boose <pestpestthechicken@yahoo.com>
2024-11-22 22:05:36 -0800

    Typecasted some stuff.

--- src/main.c
+++ src/main.c
@@ -4,16 +4,16 @@
 int main(void) {
     seed_rand();
     Option options[] = {
-        {.msg = "1. Play Game", .sequence = "1"},
-        {.msg = "2. Print Rules", .sequence = "2"},
-        {.msg = "3. Exit", .sequence = "3"},
+        {.msg = (unsigned char *)"1. Play Game", .sequence = (unsigned char *)"1"},
+        {.msg = (unsigned char *)"2. Print Rules", .sequence = (unsigned char *)"2"},
+        {.msg = (unsigned char *)"3. Exit", .sequence = (unsigned char *)"3"},
     };
 
     size_t selection;
     do {
         CLEAR_SCREEN();
         puts("To select an option, press the associated number key, use the arrow keys, or press J or K.\n\nThen, press [Enter] to confirm and submit your selection.\n");
-        selection = menu(options, "Poker Main Menu", 3);
+        selection = menu(options, (unsigned char *)"Poker Main Menu", 3);
         switch (selection) {
             case 0:
                 play_poker();
